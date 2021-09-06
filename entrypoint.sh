891c6018e343661d2684f218cfd9c555571b7018
Dvin <dvyu@yandex.ru>
2021-09-06 22:14:16 +0500

    debug

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
 # exit when any command fails
-set -e
+#set -e
 
 # keep track of the last executed command
-trap 'last_command=$current_command; current_command=$BASH_COMMAND' DEBUG
+#trap 'last_command=$current_command; current_command=$BASH_COMMAND' DEBUG
 # echo an error message before exiting
-trap 'echo "\"${last_command}\" command filed with exit code $?."' EXIT
+#trap 'echo "\"${last_command}\" command filed with exit code $?."' EXIT
 
 # Root Cettificate to trust SSL Docker Host
 echo "$ROOT_CA_CERTIFICATE" | base64 --decode > /tmp/root-ca.crt
@@ -17,6 +17,6 @@ echo "$CLIENT_CERTIFICATE_KEY" | base64 --decode > /tmp/client.key
 
 docker version
 
-#docker-compose version
+docker-compose version
 
 docker --host $DOCKER_VM_HOST --tlscacert /tmp/root-ca.crt --tlscert /tmp/client.crt --tlskey /tmp/client.key --tlsverify ps -a
