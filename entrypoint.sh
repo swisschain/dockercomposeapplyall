d3b58c27a1279df7a5068bc7a0c8182413a21bf0
Dvin <dvyu@yandex.ru>
2021-09-07 11:36:23 +0500

    debug

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,24 +1,24 @@
 #!/bin/sh
 
 # exit when any command fails
-#set -e
+set -e
 
 # keep track of the last executed command
-#trap 'last_command=$current_command; current_command=$BASH_COMMAND' DEBUG
+trap 'last_command=$current_command; current_command=$BASH_COMMAND' DEBUG
 # echo an error message before exiting
-#trap 'echo "\"${last_command}\" command filed with exit code $?."' EXIT
+trap 'echo "\"${last_command}\" command filed with exit code $?."' EXIT
 
 # Root Cettificate to trust SSL Docker Host
-#echo "$ROOT_CA_CERTIFICATE" | base64 --decode > /tmp/root-ca.crt
+echo "$ROOT_CA_CERTIFICATE" | base64 --decode > /tmp/root-ca.crt
 # Client Cettificate
-#echo "$CLIENT_CERTIFICATE" | base64 --decode > /tmp/client.crt
+echo "$CLIENT_CERTIFICATE" | base64 --decode > /tmp/client.crt
 # Client Cettificate Private Key
-#echo "$CLIENT_CERTIFICATE_KEY" | base64 --decode > /tmp/client.key
+echo "$CLIENT_CERTIFICATE_KEY" | base64 --decode > /tmp/client.key
 
-#ls -la /tmp
+ls -la /tmp
 
 docker version
 
-#docker-compose version
+docker-compose version
 
-#docker --host $DOCKER_VM_HOST --tlscacert /tmp/root-ca.crt --tlscert /tmp/client.crt --tlskey /tmp/client.key --tlsverify ps -a
+docker --host $DOCKER_VM_HOST --tlscacert /tmp/root-ca.crt --tlscert /tmp/client.crt --tlskey /tmp/client.key --tlsverify ps -a
