682be0c4f7eaec078aa2535da51625f76fed58ae
Dvin <dvyu@yandex.ru>
2021-09-07 11:37:37 +0500

    debug

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,11 +3,6 @@
 # exit when any command fails
 set -e
 
-# keep track of the last executed command
-trap 'last_command=$current_command; current_command=$BASH_COMMAND' DEBUG
-# echo an error message before exiting
-trap 'echo "\"${last_command}\" command filed with exit code $?."' EXIT
-
 # Root Cettificate to trust SSL Docker Host
 echo "$ROOT_CA_CERTIFICATE" | base64 --decode > /tmp/root-ca.crt
 # Client Cettificate
