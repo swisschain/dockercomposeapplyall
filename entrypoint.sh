18bb887c9eee73e8f91bf9440c4316031eeaa7d6
Dvin <dvyu@yandex.ru>
2021-09-22 20:17:32 +0500

    try 'EOF'

--- entrypoint.sh
+++ entrypoint.sh
@@ -13,10 +13,10 @@ case $MODE in
     echo $SSH_KNOW_HOST > /known_hosts
     echo run command
     #ssh -i /id_rsa -o UserKnownHostsFile=/known_hosts $DOCKER_VM_HOST docker-compose -f $SERVICE_YAML_FILE up -d
-    echo hostname > /commands
-    echo docker-compose -f $SERVICE_YAML_FILE up -d  >> /commands
-    cat /commands
-    ssh -i /id_rsa -o UserKnownHostsFile=/known_hosts $DOCKER_VM_HOST << /commands
+    ssh -i /id_rsa -o UserKnownHostsFile=/known_hosts $DOCKER_VM_HOST << 'EOF'
+       hostname
+       docker-compose -f $SERVICE_YAML_FILE up -d
+    EOF
     ;;
 
   SWARM)
