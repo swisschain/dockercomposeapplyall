be03b0587e78b072279233dbdbdd0e523976ecd9
Dvin <dvyu@yandex.ru>
2021-09-22 17:08:55 +0500

    run up -d

--- entrypoint.sh
+++ entrypoint.sh
@@ -15,9 +15,9 @@ case $MODE in
     echo $SSH_KNOW_HOST > ~/.ssh/known_hosts
     echo run command
     #ssh -i ~/.ssh/id_rsa $DOCKER_VM_HOST docker-compose -f $SERVICE_YAML_FILE up -d
-    #ssh -i ~/.ssh/id_rsa -o StrictHostKeyChecking=no $DOCKER_VM_HOST docker-compose -f $SERVICE_YAML_FILE up -d
-    ssh -i ~/.ssh/id_rsa -o StrictHostKeyChecking=no $DOCKER_VM_HOST pwd
-    ssh -i ~/.ssh/id_rsa -o StrictHostKeyChecking=no $DOCKER_VM_HOST ls -la 
+    ssh -i ~/.ssh/id_rsa -o StrictHostKeyChecking=no $DOCKER_VM_HOST docker-compose -f $SERVICE_YAML_FILE up -d
+    #ssh -i ~/.ssh/id_rsa -o StrictHostKeyChecking=no $DOCKER_VM_HOST pwd
+    #ssh -i ~/.ssh/id_rsa -o StrictHostKeyChecking=no $DOCKER_VM_HOST ls -la 
     #ssh $DOCKER_VM_HOST << EOF
     #  hostname
     #EOF
